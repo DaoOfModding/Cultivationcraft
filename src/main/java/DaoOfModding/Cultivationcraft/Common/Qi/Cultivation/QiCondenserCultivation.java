4c3b49d72c4056854c745d10f9ed383b7f9cf75b
Bababaa <bababaa@evalpowar.com>
2023-11-15 11:29:14 +1100

    Stored the amount of specifics elements cultivated for QiCondensers

--- src/main/java/DaoOfModding/Cultivationcraft/Common/Qi/Cultivation/QiCondenserCultivation.java
+++ src/main/java/DaoOfModding/Cultivationcraft/Common/Qi/Cultivation/QiCondenserCultivation.java
@@ -2,11 +2,17 @@ package DaoOfModding.Cultivationcraft.Common.Qi.Cultivation;
 
 import DaoOfModding.Cultivationcraft.Client.GUI.Screens.CultivationTypeScreens.QiCondenserScreen;
 import DaoOfModding.Cultivationcraft.Common.Capabilities.CultivatorStats.CultivatorStats;
+import DaoOfModding.Cultivationcraft.Common.Qi.Elements.Elements;
 import DaoOfModding.Cultivationcraft.Common.Qi.Techniques.PassiveTechniques.CultivationPassives.FoundationPassive;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.player.Player;
+
+import java.util.HashMap;
 
 public class QiCondenserCultivation extends CultivationType
 {
+    protected HashMap<ResourceLocation, float> elementProgression = new HashMap();
+
     public QiCondenserCultivation()
     {
         this(1);
@@ -39,5 +45,37 @@ public class QiCondenserCultivation extends CultivationType
 
             CultivatorStats.getCultivatorStats(player).setCultivation(newCultivation);
         }
+    }
+
+    @Override
+    public float progressCultivation(Player player, float Qi, ResourceLocation element)
+    {
+        float changedQi = super.progressCultivation(player, Qi, element);
+
+        float usedQi = Qi - changedQi;
+
+        if (!elementProgression.containsKey(element))
+            elementProgression.put(element, 0);
+
+        elementProgression.put(element, elementProgression.get(element) + usedQi);
+
+        return changedQi;
+    }
+
+    public ResourceLocation getCurrentElementFocus()
+    {
+        float amount = 0;
+        ResourceLocation currentElement = Elements.noElement;
+
+        for (ResourceLocation element : elementProgression.keySet())
+        {
+            if (elementProgression.get(element) > amount)
+            {
+                amount = elementProgression.get(element);
+                currentElement = element;
+            }
+        }
+
+        return currentElement;
     }
 }
